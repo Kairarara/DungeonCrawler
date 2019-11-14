505885560466b61900ccf48804ddd427f63e059a
Pierluigi Caruso <PierluigiC97@gmail.com>
2019-11-14 23:04:30 +0100

    Updates

---
+++ precache-manifest.b1b11f06c3805cdd1608e4fa179651e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e639b9dc2a5fb5a0ad1fa0cd9489f20a",
+    "url": "/DungeonCrawler/index.html"
+  },
+  {
+    "revision": "318beb933c15da64c80d",
+    "url": "/DungeonCrawler/static/css/main.7c865ca2.chunk.css"
+  },
+  {
+    "revision": "e125073f357739afd484",
+    "url": "/DungeonCrawler/static/js/2.c7f2e08b.chunk.js"
+  },
+  {
+    "revision": "318beb933c15da64c80d",
+    "url": "/DungeonCrawler/static/js/main.4a20be38.chunk.js"
+  },
+  {
+    "revision": "a4debe7eb4cb03ca3d96",
+    "url": "/DungeonCrawler/static/js/runtime-main.3942dc79.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/DungeonCrawler/static/media/logo.25bf045c.svg"
+  }
+]);
